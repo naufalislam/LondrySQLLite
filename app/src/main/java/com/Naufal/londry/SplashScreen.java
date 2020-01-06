f8d882e1941ef8e478393f7e3916bb00dfd4351a
Raya <ryndrad@gmail.com>
2020-01-07 01:22:30 +0700

    Initial commit

---
+++ app/src/main/java/com/Naufal/londry/SplashScreen.java
@@ -0,0 +1,40 @@
+package com.Naufal.londry;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private int waktu_loading=4000;
+
+    //4000=4 detik
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        getSupportActionBar().hide();
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                //setelah loading maka akan langsung berpindah ke home activity
+                Intent home=new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(home);
+                finish();
+
+            }
+        },waktu_loading);
+    }
+
+
+
+
+
+}
+
